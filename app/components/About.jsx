8708410165a73f0fba46c6342059cdbe8e1a23b8
codemonkeywebcorder <nakamuradev001@gamil.com>
2024-02-19 05:09:19 +0900

    update

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -9,8 +9,6 @@ var About = (props) => {
       <p>This is a simple weather application built on <a href="https://facebook.github.io/react/" target="_blank">React</a>.</p>
       <p>This app utilizes <a href="http://openweathermap.org/" target="_blank">OpenWeatherMaps</a> API to pull and return current weather data from around the world.</p>
       <p>The source code for this application can be viewed on <a href="https://github.com/bmorelli25/ReactWeather" target="_blank">Github.</a></p>
-      <p>The source code for this application can be viewed on <a href="https://github.com/bmorelli25/ReactWeather" target="_blank">Github</a></p>
-      <p>The source code for this application can be viewed on <a href="https://github.com/bmorelli25/ReactWeather" target="_blank">Github/.</a></p>
     </div>
   );
 };
